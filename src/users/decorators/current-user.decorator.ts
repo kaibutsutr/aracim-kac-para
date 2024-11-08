c567cd610dc22a771f12b3dd7c15a3b1a8d36e3e
kaibutsutr <76039421+kaibutsutr@users.noreply.github.com>
2024-11-08 22:36:02 +0300

    121

--- src/users/decorators/current-user.decorator.ts
+++ src/users/decorators/current-user.decorator.ts
@@ -3,9 +3,12 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 export const CurrentUser = createParamDecorator(
   //create a param decorator with custom values
   (data: never, context: ExecutionContext) => {
+    //ExecutionContext is same as Request but it also supports websocket,geodata etc so fancy syntax
+    // this decorator just reads currentuser part of the request, currentuser part comes from our custom interceptor
     // accept any kind of data, context is same as Request but more general, not just http requests.
     //data:never means decorator doesnt take any arguments
     const request = context.switchToHttp().getRequest(); // convert context to http object and get the request
-    console.log(request.session.userId());
+    // thanks to our custom interceptor our request object has a currentUser info. So we can grab id from it
+    return request.session.currentUser;
   },
 );
