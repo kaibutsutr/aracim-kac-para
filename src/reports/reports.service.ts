5646fc82b02d533b5b1d4beb4819d3612c186ed2
kaibutsutr <76039421+kaibutsutr@users.noreply.github.com>
2024-11-10 00:36:39 +0300

    repo

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { Report } from './report.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
+  create() {}
+}
